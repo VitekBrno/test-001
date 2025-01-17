338de42563258fba24b248a10e96c5cfe25714f3
VitekBrno <dev@svobodamail.cz>
2025-01-17 09:20:34 +0100

    Update i18n.ts

--- i18n.ts
+++ i18n.ts
@@ -18,8 +18,8 @@ declare module 'vue-i18n/types' {
  *****************************************************************************/
 Vue.use(VueI18n);
 const i18n = new VueI18n({
-  locale: process.env.VUE_APP_I18N_LOCALE || 'cs',
-  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'cs',
+  locale: process.env.VUE_APP_I18N_LOCALE || 'en',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'en',
   messages: {
     cs: czDictionary,
   },
