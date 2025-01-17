194c8bf93454bd8d3dc304d665b3e9dae545b9d1
VitekBrno <dev@svobodamail.cz>
2025-01-17 09:15:33 +0100

    Create i18n.ts
    first commit

---
+++ i18n.ts
@@ -0,0 +1,28 @@
+import Vue from 'vue';
+import VueI18n from 'vue-i18n';
+import czDictionary from './locales/cz.json';
+
+/**
+ * Overloads VueI18n interface to avoid needing to cast return value to string.
+ * @see https://github.com/kazupon/vue-i18n/issues/410
+ */
+declare module 'vue-i18n/types' {
+  export default class VueI18n {
+    t(key: Path, locale: Locale, values?: Values): string;
+    t(key: Path, values?: Values): string;
+  }
+}
+
+/******************************************************************************
+ * VUE-I18N, https://github.com/kazupon/vue-i18n
+ *****************************************************************************/
+Vue.use(VueI18n);
+const i18n = new VueI18n({
+  locale: process.env.VUE_APP_I18N_LOCALE || 'cs',
+  fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || 'cs',
+  messages: {
+    cs: czDictionary,
+  },
+});
+
+export default i18n;
